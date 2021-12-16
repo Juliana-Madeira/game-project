bb0af14ef867a04b01b81d71426ad5392ba7e409
Sergio <juliwood1980@gmail.com>
2021-12-16 19:43:49 -0300

    btn started game with reset

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 window.onload = () => {
-   
 
-    //function startGame(){};
 
-    class LocalGame{
-        constructor(){
+    class LocalGame {
+        constructor() {
             this.canvas = document.getElementById('canvas');
             this.context = this.canvas.getContext('2d');
             this.obstacles = [];    //quero vários obstaculos
@@ -12,103 +10,129 @@ window.onload = () => {
             this.frames = 0;     //cada quadro criador dos obstaculos
             this.points = 0;   //começa com 0 pontos
             this.numberCrashed = 0;
-            
+            this.gameStarted = false;
+
+
         }
         start = () => {
-            this.intervalId = setInterval (updateLocalGame, 60);
+            this.gameStarted = true;
+            this.intervalId = setInterval(updateLocalGame, 60);
         }
 
         clear = () => {
-            this.context.clearRect (0, 0, this.canvas.width, this.canvas.height);
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
 
         updateObstacles = () => {
-    
-            if (this.frames % 80 === 0){
-                
+
+            if (this.frames % 80 === 0) {
+
                 const randomY = Math.floor(Math.random() * this.canvas.height)
-                this.obstacles.push(new Obstacle(this.canvas.width, randomY ))
-                
+                this.obstacles.push(new Obstacle(this.canvas.width, randomY))
+
             }
         }
 
         updatePremium = () => {
 
-            if (this.frames % 60 === 0){
+            if (this.frames % 60 === 0) {
 
                 const randomPremium = Math.floor(Math.random() * this.canvas.height)
                 this.premium.push(new Premium(this.canvas.width, randomPremium))
             }
         }
-        
+
         stop = () => {
             console.log('entrei no stop')
             clearInterval(this.intervalId)
         }
 
+        gameOver = () => {
+            this.stop()
+            this.clear()
+            let img = new Image();
+            img.onload = () => {
+                this.context.drawImage(img, 0, 0, 800, 500)
+            }
+            img.src = './images/dumbledore-game-over.jpg'
+            
+            console.log('deu game over')
+
+        }
         
+        reset = () => {
+            this.stop();
+            this.obstacles = [];    //quero vários obstaculos
+            this.premium = [];    //varios premios (socks)
+            this.frames = 0;     //cada quadro criador dos obstaculos
+            this.points = 0;   //começa com 0 pontos
+            this.numberCrashed = 0;
+        }
+
     };
     const localGame = new LocalGame();
 
 
 
     class BackgroundGame {
-        constructor () {
+        constructor() {
             this.img = new Image()
             this.img.src = './images/hogwarts-background.jpg';     /*fundo do jogo, Hogwars aqui*/
-            
+
         }
-        draw = () => { 
+        draw = () => {
             localGame.context.drawImage(this.img, 0, 0, 800, 500);
         }
     };
-     const background = new BackgroundGame();
+    const background = new BackgroundGame();
 
 
 
 
     class Dobby {
-        constructor(x, y, width, height){         //onde o Dobby estará, posição
+        constructor(x, y, width, height) {         //onde o Dobby estará, posição
             this.posX = x;
             this.posY = y;
+            this.resetX = x;
+            this.resetY = y;
             this.width = width;
-            this.height = height; 
+            this.height = height;
             this.img = new Image();
             this.img.src = './images/dobbyPNG.png';
             this.speed = 10;   //velocidade de movimentação do Dobby
-            
+
         }
         draw = () => {
-            localGame.context.drawImage(this.img,this.posX, this.posY, this.width, this.height);
+            localGame.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
         }
 
         move = (command) => {
-            switch (command){
+            switch (command) {
                 case 'ArrowUp':
-                    if (this.posY > 15){
-                    this.posY -= this.speed;
+                    if (this.posY > 15) {
+                        this.posY -= this.speed;
                     }
                     break;
                 case 'ArrowDown':
-                    if (this.posY < 500 - 15 - this.height){
-                    this.posY += this.speed;
+                    if (this.posY < 500 - 15 - this.height) {
+                        this.posY += this.speed;
                     }
                     break;
                 case 'ArrowLeft':
-                    if (this.posX > 15){
-                    this.posX -= this.speed;
+                    if (this.posX > 15) {
+                        this.posX -= this.speed;
                     }
                     break;
                 case 'ArrowRight':
-                    if (this.posX < 800 - 15 - this.height){
-                    this.posX += this.speed;
+                    if (this.posX < 800 - 15 - this.height) {
+                        this.posX += this.speed;
                     }
-                    break; 
+                    break;
             }
         }
-        
+
         top = () => {
-            return this.posY 
+            return this.posY
         }
         bottom = () => {
             return this.posY + this.height
@@ -121,42 +145,47 @@ window.onload = () => {
         }
 
 
-        crashWith(obstacles){
+        crashWith(obstacles) {
             const freeLeft = this.left() > obstacles.right();
             const freeRight = this.right() < obstacles.left();
             const freeTop = this.top() > obstacles.bottom();
-            const freeBottom = this.bottom() < obstacles.top(); 
+            const freeBottom = this.bottom() < obstacles.top();
 
             return (!(freeLeft || freeRight || freeTop || freeBottom))
+        }
+        
+        reset = () => {
+            this.posX = this.resetX;
+            this.posY = this.resetY;
         }
 
     };
 
-     const dobby = new Dobby(50, 200, 80, 80); 
+    const dobby = new Dobby(50, 200, 80, 80);
 
 
 
     class Obstacle {
-        constructor (x, y){
+        constructor(x, y) {
             this.posX = x;
             this.posY = y;
             this.width = 60;
             this.height = 60;
             this.speed = 5;
-            this.img = new Image(); 
+            this.img = new Image();
             this.img.src = './images/voldemort-obstacle.png'
         }
 
         draw = () => {
-           localGame.context.drawImage(this.img,this.posX, this.posY, this.width, this.height);
+            localGame.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
         }
 
-        updatePosition = () =>{             //alteração de posição do obstáculo, cada posX - cada speed, vai movimentando
+        updatePosition = () => {             //alteração de posição do obstáculo, cada posX - cada speed, vai movimentando
             this.posX -= this.speed;
         }
 
         top = () => {
-            return this.posY 
+            return this.posY
         }
         bottom = () => {
             return this.posY + this.height
@@ -170,31 +199,31 @@ window.onload = () => {
 
     };
 
-    const obst = new Obstacle (600,300)
+    const obst = new Obstacle(600, 300)
 
 
     class Premium {
-        constructor (x, y){
+        constructor(x, y) {
             this.posX = x;
             this.posY = y;
             this.width = 35;
             this.height = 35;
             this.speed = 6;
-            this.img = new Image(); 
+            this.img = new Image();
             this.img.src = './images/socks.png'
         }
 
         draw = () => {
-           localGame.context.drawImage(this.img,this.posX, this.posY, this.width, this.height);
+            localGame.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
         }
 
 
-        updatePosition = () =>{             //alteração de posição do premio, cada posX - cada speed, vai movimentando
+        updatePosition = () => {             //alteração de posição do premio, cada posX - cada speed, vai movimentando
             this.posX -= this.speed;
         }
 
         top = () => {
-            return this.posY 
+            return this.posY
         }
         bottom = () => {
             return this.posY + this.height
@@ -209,18 +238,18 @@ window.onload = () => {
 
     };
 
-    const premium = new Premium (500,500)
+    const premium = new Premium(500, 500)
 
-    
-   
 
-    function updateLocalGame(){     //atualização do local game, que é a área do meu game
+
+
+    function updateLocalGame() {     //atualização do local game, que é a área do meu game
         localGame.frames += 1
         localGame.clear()
         background.draw()
         dobby.draw()
         localGame.updatePremium()
-        localGame.premium.forEach((socks) =>{
+        localGame.premium.forEach((socks) => {
             socks.updatePosition()
             socks.draw()
         })
@@ -228,49 +257,69 @@ window.onload = () => {
         localGame.obstacles.forEach((obst) => {
             obst.updatePosition()
             obst.draw()
-           
-        })            
-        checkGameOver() 
+
+        })
+        checkGameOver()
         checkLive()
     };
 
 
-    function checkLive(){
+    function checkLive() {
         const live = localGame.premium.some((socks) => {
             return dobby.crashWith(socks)
-        }); if (live){
+        });
+        if (live) {
+            localGame.premium.forEach((socks, index) => {
+                localGame.premium.splice(index, 1);
                 localGame.points += 1;
-            }
-        document.querySelector('#scoreSpan').innerText = localGame.points
-    }
 
-
-
-
-    function checkGameOver (){
-        let numberCrashed = 0;
-        const crashed = localGame.obstacles.some((obst) => {
-            return dobby.crashWith(obst)
-            }); 
-                if (crashed){
-                    localGame.obstacles.forEach((obst) => {
-                        numberCrashed += 1;
-                    })                               
-            } if (numberCrashed === 3){
-                localGame.stop()
-            }
-        
+            })
+        }
+        document.querySelector('#scoreSpan').innerText = localGame.points;
     };
 
+
+    function checkGameOver() {
+        const crashed = localGame.obstacles.some((obst) => {
+            return dobby.crashWith(obst)
+        });
+        if (crashed) {
+            console.log('aqui bateu')
+            localGame.obstacles.forEach((obst, index) => {
+                localGame.obstacles.splice(index, 1);
+                localGame.numberCrashed += 1;
+            });
+        }
+         if (localGame.numberCrashed === 3) {
+            console.log('aqui morre')
+
+            localGame.gameOver()
+
+        };
+    };
+
+    
+    function resetGame() {
+        localGame.reset()
+        dobby.reset()
+        
+    }
+    
 
 
 
     /*Event Listener todos aqui*/
     document.getElementById('start-button').onclick = () => {
+        if(!localGame.gameStarted){
+        localGame.start();
+        }
+    };
+    document.getElementById('play-again').onclick = () => {
+        resetGame();
         localGame.start();
     };
     document.addEventListener('keydown', (e) => {     //e => é o evento 
-        if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
             e.preventDefault();
         }
         dobby.move(e.key)
